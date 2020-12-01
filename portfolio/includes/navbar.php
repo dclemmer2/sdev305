a100b126e815a1974c6b4434ba9da28af9ddef4f
dclemmer2 <72044120+dclemmer2@users.noreply.github.com>
2020-11-30 21:35:58 -0800

    Add files via upload

---
+++ portfolio/includes/navbar.php
@@ -0,0 +1,12 @@
+<!--##################    NAVBAR    ##################-->
+<nav class="navbar navbar-expand-md navbar-dark fixed-top" id="navbar-site">
+    <a class="navbar-name" href="#">Dana Clemmer</a>
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item ml-auto">
+            <a class="nav-link" href="index.php">Resume/Home</a>
+        </li>
+        <li class="nav-item ml-auto">
+            <a class="nav-link" href="guestbook.php">Guestbook</a>
+        </li>
+    </ul>
+</nav>
