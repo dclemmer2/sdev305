13b605ba8f84926c1d978874f2124696b3525ee7
dclemmer2 <72044120+dclemmer2@users.noreply.github.com>
2020-12-04 17:21:18 -0800

    Update navbar.php

--- portfolio/includes/navbar.php
+++ portfolio/includes/navbar.php
@@ -1,12 +1,19 @@
 <!--##################    NAVBAR    ##################-->
 <nav class="navbar navbar-expand-md navbar-dark fixed-top" id="navbar-site">
     <a class="navbar-name" href="#">Dana Clemmer</a>
-    <ul class="navbar-nav ml-auto">
-        <li class="nav-item ml-auto">
-            <a class="nav-link" href="index.php">Resume/Home</a>
-        </li>
-        <li class="nav-item ml-auto">
-            <a class="nav-link" href="guestbook.php">Guestbook</a>
-        </li>
-    </ul>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
+            aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item ml-auto">
+                <a class="nav-link" href="index.php">Resume/Home</a>
+            </li>
+            <li class="nav-item ml-auto">
+                <a class="nav-link" href="guestbook.php">Guestbook</a>
+            </li>
+        </ul>
+    </div>
 </nav>
