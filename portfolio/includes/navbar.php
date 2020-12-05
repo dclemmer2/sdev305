5174ab35eab45461e824ffb08f9377d86f9710e5
dclemmer2 <72044120+dclemmer2@users.noreply.github.com>
2020-12-04 17:46:04 -0800

    Update navbar.php

--- portfolio/includes/navbar.php
+++ portfolio/includes/navbar.php
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarToggler">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item ml-auto">
-                <a class="nav-link" href="index.php">Resume/Home</a>
+                <a class="nav-link" href="index.php">Home</a>
             </li>
             <li class="nav-item ml-auto">
                 <a class="nav-link" href="guestbook.php">Guestbook</a>
