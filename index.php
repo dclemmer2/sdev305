128829614a71fc9a2c231ec75a587819e8424234
Dana Clemmer <dclemmer2@mail.greenriver.edu>
2021-03-17 02:51:27 -0400

    deleted files

--- index.php
+++ index.php
@@ -1,137 +1,65 @@
-<!--Landing Page-->
-
 <?php
-/*
- * Dana Clemmer
- * index.php
- */
+//This is my CONTROLLER
 
-$page_title = 'Home';
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-/*//Login info
-include('includes/loginPath.php');*/
+//Require files
+require_once('vendor/autoload.php');
 
-//Add Header
-include("includes/head.php");
-?>
+//Connect to database using PDO
+require($_SERVER['DOCUMENT_ROOT'] . '/../config.php');
 
-<!--##################    NAVBAR    ##################-->
-<nav class="navbar navbar-expand-md navbar-dark fixed-top" id="navbar-site">
-    <a class="navbar-name" href="#">Dana Clemmer</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
-            aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+//Start a session
+session_start();
 
-    <div class="collapse navbar-collapse" id="navbarToggler">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item ml-auto">
-                <a class="nav-link" href="#connect">Connect</a>
-            </li>
-            <li class="nav-item ml-auto">
-                <a class="nav-link" href="#about-index">About</a>
-            </li>
-            <li class="nav-item ml-auto">
-                <a class="nav-link" href="#projects">Projects</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+//Create an instance of the Base class
+$f3 = Base::instance();
 
-<!--##################    HEADER    ##################-->
-<header id="index-header">
-    <div class="header rounded" id="connect">
-        <div class="idCard rounded" id="index">
-            <img class="rounded-circle" src="images/profilepic.jpg"
-                 alt="Profile picture">
-            <h5 class="name font-weight-bold pb-3">Dana Clemmer</h5>
-            <div class="header-body">
-                <ul class="header-contact">
-                    <li>
-                        <i class="fa fa-envelope" aria-hidden="true"></i><a
-                                href="mailto:Dclemmer2@mail.greenriver.edu?subject=EmailSubjectline">Dclemmer2@mail.greenriver.edu</a>
-                    </li>
-                    <!--  <li>
-                          <i class="fa fa-phone" aria-hidden="true"></i><a href="tel:206-480-8610">206-480-8610</a>
-                      </li>-->
-                    <li>
-                        <i class="fa fa-linkedin" aria-hidden="true"></i><a
-                                href="https://www.linkedin.com/in/dana-clemmer/" target="_blank">www.linkedin.com/in/dana-clemmer</a>
-                    </li>
-                    <li>
-                        <i class="fa fa-github" aria-hidden="true"></i><a href="https://github.com/dclemmer2"
-                                                                          target="_blank">github.com/dclemmer2</a>
-                    </li>
+//add classes
+$controller = new Controller($f3);
+$dataLayer = new DataLayer($dbh);
+$validator = new Validate($dataLayer);
 
-                    <li>
-                        <i class="fa fa-address-book" aria-hidden="true"></i><a href="guestbook.php" target="_blank">Stay
-                            Connected & Sign my Guestbook!</a>
-                    </li>
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
 
-                </ul>
-            </div>
-        </div>
-    </div>
-</header>
-<br>
+//Define a default route (home page)
+$f3->route('GET /', function () {
+    global $controller;
+    $controller->home();
+});
 
+//Define a "login" route
+$f3->route('GET|POST /login', function () {
+    global $controller;
+    $controller->login();
+});
 
-<!--##################    BODY    ##################-->
-<div class="resume-container">
-    <h1 class="text-center" id="about-index">About Me</h1><br>
-    <div class="about">
-        <p class="text-center pb-3" id="about-body">My name is Dana and I am currently a Junior studying Software Development at Green River
-            College. My strengths
-            include back-end development with Java and front-end web development with HTML/CSS, JavaScript, and
-            PHP. I have worked on various projects, including creating a website with a team for a local nonprofit using
-            HTML/CSS, JavaScript, PHP, and MySQL. In another program that I created, I used Java to manage the inventory
-            of
-            a toy store by utilizing a LinkedList, ArrayList, and varying methods, such as one to find the most
-            frequently
-            purchased toy. My experience with multiple coding languages and with group projects will help me collaborate
-            with others in the work force. I am seeking an internship that I’ll be able to put my skills to use. </p>
+//Define an "admin" route
+$f3->route('GET|POST /admin', function () {
+    global $controller;
+    $controller->admin();
+});
 
-        <a class="btn resume-btn font-weight-bold d-block" href="resume.php" target="_blank" role="button">Check Out My
-            Resume!</a>
-    </div>
-    <br>
-    <hr>
-    <br>
-    <br>
-    <h1 class="text-center" id="projects">Projects</h1><br>
-    <div class="project">
-        <div class="row project-images">
-            <div class="col-lg-6">
-                <a class="project-link-image" href="https://dar2.greenriverdev.com/" target="_blank"><img
-                            src="images/kentOutreach.png"
-                            class="proj-img-index center"
-                            alt="proj-img"></a>
-            </div>
-            <div class="col-lg-6">
-                <a class="project-link-image" href="https://dar2.greenriverdev.com/" target="_blank"><img
-                            src="images/kentOutreach2.png"
-                            class="proj-img-index center"
-                            alt="proj-img"></a>
-            </div>
-        </div>
+//Define an "guestbook" route
+$f3->route('GET|POST /guestbook', function () {
+    global $controller;
+    $controller->guestbook();
+});
 
-        <div class="project-body">
-            <p><span class="font-weight-bold">Outreach Services Management System Project</span> - Auburn, WA<span
-                        class="float-right">Fall 2020</span></p>
-            <p>Green River College</p>
-            <ul>
-                <li>As a team (4 people), we created a website for a nonprofit organization that provides various
-                    services.
-                    Services are limited, thus customers must meet set qualifications to be helped. There is a limited
-                    amount of staff on hand, so my team created a form that customers can find through the website where
-                    they can fill out what services they need if they meet the given qualifications.
-                </li>
-                <li>Utilized HTML5, CSS3, JavaScript, PHP, and MySQL in PHPStorm</li>
-                <li><a id="project-link" href="https://dar2.greenriverdev.com/" target="_blank">Check it out!</a></li>
-            </ul>
-        </div>
-    </div>
-</div>
+//Define a "submission" route
+$f3->route('GET|POST /submission', function () {
+    global $controller;
+    $controller->submission();
+});
 
-<!--##################    FOOTER SECTION    ##################-->
-<?php include('includes/footer.php'); ?>
+//Define a "logout" route
+$f3->route('GET|POST /logout', function () {
+    global $controller;
+    $controller->logout();
+});
+
+//Run fat free
+$f3->run();
